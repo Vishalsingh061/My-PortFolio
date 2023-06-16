6abd37920715f50aacff4b0d06516122853465ec
Mohammad Nazir Sharifi <nazirsharifi19@gmail.com>
2023-06-16 12:43:26 +0300

    general css style added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
-function App() {
+import React, {Fragment} from 'react';
+import Header from './components/header/Header';
+import Nav from './components/nav/Nav';
+import About from './components/about/About';
+import Experience from './components/experience/Experience';
+import Services from './components/services/Services';
+import Portfolio from './components/portfolio/Portfolio';
+import Contact from './components/contact/Contact';
+import Footer from './components/footer/Footer';
+
+const App = () => {
   return (
-    <div>
-      <h2>Let's get started!</h2>
-    </div>
-  );
+    <Fragment>
+      <Header/>
+      <Nav/>
+      <About/>
+      <Experience/>
+      <Services/>
+      <Portfolio/>
+      <Contact/>
+      <Footer/>
+    </Fragment>
+  )
 }
 
-export default App;
+export default App
