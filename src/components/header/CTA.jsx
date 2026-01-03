5bee62d9e6b934c11519825eb727f6b42e9bf064
Vishal Singh <vishalpratap906@gmail.com>
2026-01-03 15:12:49 +0530

    Update CV download link

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const CTA = () => {
   return (
     <div className='cta'>
-      <a href="https://drive.google.com/file/d/1lKG-TgFiqDao8GGanVfTJ82NROe9iPGH/view?usp=sharing" download className='btn'>Download CV</a>
+      <a href="https://drive.google.com/file/d/1RiXsckeGDpY4qx4_ZDiie2ztXr8_Kg7u/view?usp=sharing" download className='btn'>Download CV</a>
       <a href="#contact" className='btn btn-primary'>Let's Talk</a>
     </div>
   )
